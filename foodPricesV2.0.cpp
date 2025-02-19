52efe921ea0c3493f7a4ab0047c4361310b7d38b
WilhelMark <46066617+WilhelMark@users.noreply.github.com>
2025-02-19 18:03:48 +0300

    shopping vector

--- foodPricesV2.0.cpp
+++ foodPricesV2.0.cpp
@@ -25,3 +25,21 @@ int main() {
     // Вектор индексов купленных товаров
     std::vector<int> items;
     int numItems;
+    
+    // Запрашиваем количество покупок
+    std::cout << "Введите количество покупок: ";
+    std::cin >> numItems;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Очищаем буфер ввода
+
+    // Заполняем вектор покупок
+    std::cout << "Введите индексы товаров через пробел: ";
+    std::string line;
+    std::getline(std::cin, line);  // Читаем строку с индексами
+    std::stringstream ss(line);     // Создаем строковый поток
+    int itemIndex;
+    while (ss >> itemIndex) {       // Читаем индексы из потока
+        items.push_back(itemIndex);
+    }
+
+    // Переменная для хранения итоговой стоимости покупок.
+    float totalCost = 0.0;
