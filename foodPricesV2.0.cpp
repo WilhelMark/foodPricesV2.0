3f537dd8124e011eb80fa1150d163b5d4d505d18
WilhelMark <46066617+WilhelMark@users.noreply.github.com>
2025-02-19 18:00:15 +0300

    price request

---
+++ foodPricesV2.0.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <vector>
+#include <sstream> // Для std::stringstream
+#include <limits> // Для std::numeric_limits
+
+int main() {
+    // Вектор цен на продукты
+    std::vector<float> prices;
+    int numPrices;
+
+    // Запрашиваем количество цен
+    std::cout << "Введите количество цен: ";
+    std::cin >> numPrices;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Очищаем буфер ввода
