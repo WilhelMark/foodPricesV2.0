bd7f0c7086c299c4cbc2c37a8b12153a6212d89d
WilhelMark <46066617+WilhelMark@users.noreply.github.com>
2025-02-19 18:02:07 +0300

    vector filling

--- foodPricesV2.0.cpp
+++ foodPricesV2.0.cpp
@@ -12,3 +12,16 @@ int main() {
     std::cout << "Введите количество цен: ";
     std::cin >> numPrices;
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Очищаем буфер ввода
+
+        // Заполняем вектор цен
+    for (int i = 0; i < numPrices; ++i) {
+        float price;
+        std::cout << "Введите цену для товара " << i + 1 << ": ";
+        std::cin >> price;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Очищаем буфер ввода
+        prices.push_back(price);
+    }
+
+    // Вектор индексов купленных товаров
+    std::vector<int> items;
+    int numItems;
